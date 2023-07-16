130b4868d1cb5c8f7939985d20d7c03ce167740c
coxxy666 <ezeiyokecosmas@gmail.com>
2023-07-16 05:45:34 -0700

    Write a C program that prints exactly and that piece of art is useful

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,8 @@
 
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDERR_FILENO, message, strlen(message));
+	char *mess = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(STDERR_FILENO, mess, strlen(mess));
 	return (1);
 }
